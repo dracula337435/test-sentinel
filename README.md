8b11468081d993617b526ad6e97180a9040478f2
dk <dracula337435@163.com>
2019-08-09 17:10:27 +0800

    对接nacos的实验

--- README.md
+++ README.md
@@ -10,3 +10,19 @@ java -Dserver.port=8080 -Dcsp.sentinel.dashboard.server=localhost:8080 -Dproject
 ```
 -Dcsp.sentinel.dashboard.server=localhost:8080 -Dproject.name=some-name
 ```
+
+对接```nacos```后，参考```demo```即可配置好，此时```nacos```和```dashboard```均可实时修改规则  
+在```nacos```修改后可见日志类似于：
+```
+2019-08-09 17:02:21.488  INFO 18180 --- [-localhost_8848] c.a.n.client.config.impl.ClientWorker    : [fixed-localhost_8848] [polling-resp] config changed. dataId=test-sentinel, group=DEFAULT_GROUP
+2019-08-09 17:02:21.495  INFO 18180 --- [-localhost_8848] c.a.n.client.config.impl.ClientWorker    : [fixed-localhost_8848] [data-received] dataId=test-sentinel, group=DEFAULT_GROUP, tenant=null, md5=4f5643ac25a487a2caba34cd56d7c2f6, content=[
+  {
+    "resource": "HelloWorld",
+    "controlBehavior": 0,
+    "count": 99.0,
+    "grade": 1,
+   ...
+2019-08-09 17:02:21.495  INFO 18180 --- [-localhost_8848] c.a.nacos.client.config.impl.CacheData   : [fixed-localhost_8848] [notify-listener] time cost=0ms in ClientWorker, dataId=test-sentinel, group=DEFAULT_GROUP, md5=4f5643ac25a487a2caba34cd56d7c2f6, listener=com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource$1@212f6381 
+2019-08-09 17:02:21.496  INFO 18180 --- [update-thread-1] c.a.nacos.client.config.impl.CacheData   : [fixed-localhost_8848] [notify-ok] dataId=test-sentinel, group=DEFAULT_GROUP, md5=4f5643ac25a487a2caba34cd56d7c2f6, listener=com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource$1@212f6381 
+```
+在```dashboard```修改，在```nacos```不会有体现；反之可以
---
+++ src/test/java/org/dracula/test/sentinel/NacosTest.java
@@ -0,0 +1,78 @@
+package org.dracula.test.sentinel;
+
+import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
+import com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
+import org.dracula.test.sentinel.service.TestService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+/**
+ * @author dk
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MainApp.class)
+public class NacosTest {
+
+    @Autowired
+    private TestService testService;
+
+    static {
+        String remoteAddress = "localhost";
+        String groupId = "DEFAULT_GROUP";
+        String dataId = "test-sentinel";
+        //放入配置
+        final String rule = "[\n"
+                + "  {\n"
+                + "    \"resource\": \"HelloWorld\",\n"
+                + "    \"controlBehavior\": 0,\n"   // 0. default(reject directly), 1. warm up, 2. rate limiter, 3. warm up + rate limiter
+                + "    \"count\": 5.0,\n"
+                + "    \"grade\": 1,\n" //0: thread count, 1: QPS
+                + "    \"limitApp\": \"default\",\n"
+                + "    \"strategy\": 0\n"
+                + "  }\n"
+                + "]";
+        try {
+            ConfigService configService = NacosFactory.createConfigService(remoteAddress);
+            System.out.println(configService.publishConfig(dataId, groupId, rule));
+        } catch (NacosException e) {
+            e.printStackTrace();
+        }
+        //得到配置，并置流控规则生效
+        ReadableDataSource<String, List<FlowRule>> flowRuleDataSource = new NacosDataSource<>(remoteAddress, groupId, dataId,
+                source -> JSON.parseObject(source, new TypeReference<List<FlowRule>>() {
+                }));
+        FlowRuleManager.register2Property(flowRuleDataSource.getProperty());
+    }
+
+    @Test
+    public void test(){
+        while(true){
+            for(int i=0; i<100; i++){
+                try {
+                    System.out.println(testService.test());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
